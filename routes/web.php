fb87016e0cdacb90b5bb8b769ca665cce8afa0ca
unknown <mohammad.ali.romani@gmail.com>
2024-06-20 14:32:56 +0300

    add folder view and add route admin

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::view('/home','home');
+
+
+
+
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
