eba706437a872e650a05e93e2a009ecf19292663
jawad zahwe <jawadzahwe225@gmail.com>
2024-06-23 07:13:19 +0300

    add links

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -67,7 +67,5 @@
     <!-- JS -->
     <script src="{{asset('register/js/jquery.min.js')}}"></script>
     <script src="{{asset('register/js/main.js')}}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -15,9 +15,9 @@
 
         <!-- Icon Font Stylesheet -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
         <!-- Libraries Stylesheet -->
+        <link href="{{asset('bootstrap/bootstrap-icons.css')}}" rel="stylesheet">
         <link href="{{asset('lib/lightbox.min.css')}}" rel="stylesheet">
         <link href="{{asset('lib/owl.carousel.min.css')}}" rel="stylesheet">
 
@@ -45,8 +45,8 @@
 
         
     <!-- JavaScript Libraries -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('jquery/jquery.js')}}"></script>
+    <script src="{{asset('bootstrap/bootstrap.js')}}"></script>
     <script src="{{asset('lib/easing.min.js')}}"></script>
     <script src="{{asset('lib/waypoints.min.js')}}"></script>
     <script src="{{asset('lib/lightbox.min.js')}}"></script>
